99b3502adba09f130cf5504992b4ba3024975893
jallen70 <joy.allen@ncl.ac.uk>
2017-08-14 09:05:33 +0100

    update

--- functions/univ_regression.R
+++ functions/univ_regression.R
@@ -35,14 +35,28 @@ univ_regression <- function(vari, vari_name, mydata1){
     }
 
 
+  # calculate pseudo R-squared - McFaddon
+  
+  nullmod <- glm(outcome~1, family="binomial", data = mydata1)
+  #lrtest(nullmod, m)
+  anova(m, test = "Chisq") # likelihood ratio test statistics is chi2 = 11.096 with a p value of 0.00087 therefore
+  # we have relatively strong evidence in favour of rejecting Null hypo.  
+  #drop1(m,test = "Chisq")
+  1-logLik(m)/logLik(nullmod)
   
   aic=extractAIC(m)
   mydata1 <- mydata1 %>% mutate(prob = predict(m,type=c("response")))
   mydata1$prob=predict(m,type=c("response"))
-  predRisk <- predRisk(m)
+ # predRisk <- predRisk(m)
   g <- roc(outcome~prob, data = mydata1, levels=c("NMD", "MD"), ci = TRUE)
   auroc=ci.auc(g)
-  g0 <- plot.roc(outcome~ prob, data= mydata1, percent=T, ci=TRUE, levels=c("NMD", "MD"), main = (paste0(vari_name)))
+  g0 <- plot.roc(outcome~ prob, data= mydata1, percent=T, ci=TRUE, levels=c("NMD", "MD"), main = (paste0(vari_name)), 
+                 print.auc=TRUE ) #, print.auc.pattern=NULL,
+                # print.auc.x=ifelse(x$percent, 50, .5),
+               #  print.auc.y=ifelse(x$percent, 50, .5),
+                # print.auc.adj=c(0,1),
+                # print.auc.col=col,
+                # print.auc.cex=par("cex"),)
   o1a=cbind(deviance(m),aic[2],auroc[2], auroc[1], auroc[3])
   MR=cbind(coef(m),confint(m), exp(cbind(OR = coef(m),confint(m))), coef(summary(m))[,4])#odds ratio and CI
   # m1rows = cbind(mdf, o1a)
